6ca1991f09152a4df4cf89344ba6d4d66796715a
Pratap Sharma <sharma.pratap22@gmail.com>
2024-03-13 21:07:56 +0545

    Completed bloc

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,6 @@
+const String newsAPIBaseURL = 'https://newsapi.org/v2';
+const String newsAPIKey = 'ff957763c54c44d8b00e5e082bc76cb0';
+const String countryQuery = 'in';
+const String categoryQuery = 'general';
+const String kDefaultImage =
+    "https://cdn.vectorstock.com/i/preview-1x/65/30/default-image-icon-missing-picture-page-vector-40546530.jpg";
